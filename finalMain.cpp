94e701e77a7e05bcf10ef36c82e354866a474fef
Marcus <marcuscorbin2770@gmail.com>
2019-12-09 12:08:57 -0600

    added new updated files (all work with 7-bit separation so far)

--- finalMain.cpp
+++ finalMain.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <limits>
 #include <type_traits>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -14,6 +16,12 @@ void decode(void);
 string fileName, concat;
 ifstream infile; 
 
+int buffer[7];
+
+int Hmatrix[3][7] = {{0,0,0,1,1,1,1},
+					 {0,1,1,0,0,1,1},
+					 {1,0,1,0,1,0,1}};
+
 //Main Function
 int main(int argc, char *argv[])
 {
@@ -21,6 +29,16 @@ int main(int argc, char *argv[])
 	cout << "File to open: ";
 	cin >> fileName;
 	decode();
+	
+	cout << endl;
+	
+									 
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 7; j++)
+		cout << Hmatrix[i][j] << " ";
+		cout << endl;
+	}
 }
 
 void decode(void)
@@ -48,15 +66,32 @@ void decode(void)
 			a8.to_string() + a9.to_string() + a10.to_string() +
 			a11.to_string() + a12.to_string() + a13.to_string(); 
 			
-			string store[16];
+			string s[16];
 			int j = 0;
 			
 			for (int i = 0; i < 16; i++)
 			{
-				store[i] = concat.substr(j, 7);
+				string str = concat.substr(j, 7);
+				s[i] = concat.substr(j, 7);
 				
 				// TODO possibly do matrix multiplication in this 
 				// for loop
+				
+				
+				//~ for (int i = 0; i < 7; i++)
+				//~ {
+					//~ buffer[i] = str[i]-'0';
+				//~ }
+				
+				//~ cout << buffer[0];
+				//~ cout << buffer[1];
+				//~ cout << buffer[2];
+				//~ cout << buffer[3];
+				//~ cout << buffer[4];
+				//~ cout << buffer[5];
+				//~ cout << buffer[6]<< endl;
+				
+				
 				
 				j = j + 7;
 			}
@@ -64,7 +99,7 @@ void decode(void)
 			//Test print values of store
 			for (int k = 0; k < 16; k++)
 			{
-				cout << store[k] << endl;
+				cout << s[k] << endl;
 			}		
 		}
 	}
