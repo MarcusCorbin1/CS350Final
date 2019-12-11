caa690916868bfabe019f518613e2475778953e6
Marcus <marcuscorbin2770@gmail.com>
2019-12-10 22:38:00 -0600

    IT'S WORKING NOW

--- finalMain.cpp
+++ finalMain.cpp
@@ -46,6 +46,7 @@ int main(int argc, char *argv[])
 	
 	strSave = getFullBinary();
 	cout << strSave << endl;
+	outFile << strSave;
 	
 	decode(strSave);
 	
@@ -77,7 +78,7 @@ void decode(string strSave)
 	int first = 0;
 	
 	// Split the strSave into 7 bit chunks
-	for (int cnt = 0; cnt < lengthBin; cnt++)
+	for (int cnt = 0; cnt < lengthBin; cnt = cnt + 7)
 	{
 		string str = strSave.substr(first, 7);
 		
@@ -123,119 +124,6 @@ void decode(string strSave)
 		
 		first = first + 7;
 	}
-	
-	//char *c = new char[0]; // Declaration of c char pointer
-
-	
-	//Set binary values 0-13 from .enc file
-	//~ bitset<8> a0(c[0]); bitset<8> a1(c[1]); bitset<8> a2(c[2]);		
-	//~ bitset<8> a3(c[3]); bitset<8> a4(c[4]); bitset<8> a5(c[5]);
-	//~ bitset<8> a6(c[6]);
-	
-	// Concatenate the bitset together
-	//~ concat = a0.to_string() + a1.to_string() + 
-	//~ a2.to_string() + a3.to_string() + a3.to_string() + 
-	//~ a5.to_string() + a6.to_string();
-	
-	//cout << concat << "\n" <<endl;
-	
-	
-	//~ string s[16];
-	//~ int j = 0;
-	
-	//~ for (int i = 0; i < 16; i++)
-	//~ {
-		//~ //string str = concat.substr(j, 7);
-		//~ //s[i] = concat.substr(j, 7);
-		
-		//~ // TODO possibly do matrix multiplication in this 
-		//~ // for loop
-		
-		
-		//~ for (int i = 0; i < 7; i++)
-		//~ {
-			//~ buffer[i] = str[i]-'0';
-		//~ }
-		
-		//~ cout << buffer[0];
-		//~ cout << buffer[1];
-		//~ cout << buffer[2];
-		//~ cout << buffer[3];
-		//~ cout << buffer[4];
-		//~ cout << buffer[5];
-		//~ cout << buffer[6]<< endl;
-		
-		//~ int strStore[7][1];
-		//~ int k = 0;
-		
-		// Convert the string to a 1x7 Matrix
-		//~ for (int a = 0; a < 7; a++)
-		//~ {
-			//~ for (int b = 0; b < 1; b++)
-			//~ {
-				//~ // uncomment!strStore[a][b] = str[k]-48; // Minus 48 to get 0 or 1
-				//~ k++;
-			//~ }
-		//~ }
-		
-		//~ multiply (Hmatrix, strStore, res);
-		//cout << endl;
-		
-		//~ // Error Checking
-		//~ if (res[0][0] == 0 && res[1][0] == 0 && res[2][0] == 0)
-		//~ {
-			//~ string split1 = concat.substr(j,4);
-			//~ //cout << split1 << endl;
-			
-			//~ storeThis += split1;
-			
-			//~ //outFile << split1;
-			
-		//~ }
-		//~ else
-		//~ {
-			//~ //cout << "You got an error" << endl;
-			//~ err++;
-			
-		//~ }
-					
-		//~ // Test to print out matrix
-		//~ cout << "Test Matrix" << endl;
-		
-		
-		//~ for (int c = 0; c < 7; c++)
-		//~ {
-			//~ for (int d = 0; d < 1; d++)
-			//~ {
-				//~ if (strStore[c][d] == '\0')
-					//~ break;
-				//~ cout << strStore[c][d];
-			//~ }
-			//~ cout << endl; 
-		//~ }
-		
-		//~ cout << endl;
-		
-		
-		//~ cout << "Result Matrix \n";
-		//~ for (int i = 0; i < 3; i++)
-		//~ {
-			//~ for (int j = 0; j < 1; j++)
-			//~ cout << res[i][j] << " ";
-			//~ cout << "\n";
-		//~ }
-		
-		//~ cout << endl;
-		
-		
-		//~ j = j + 7;
-	//~ }
-	
-	//~ //Test print values of store
-	//~ for (int k = 0; k < 16; k++)
-	//~ {
-		//~ cout << s[k] << endl;
-	//~ }		
 }
 
 void multiply(int Hmat[3][7], int segMat[7][1], int res[3][1])
@@ -250,7 +138,7 @@ void multiply(int Hmat[3][7], int segMat[7][1], int res[3][1])
 		for (int j = 0; j < 7; j++)
 		{
 			res[i][0] += Hmat[i][j] * segMat[j][0];
-			
+		
 			//Preform Mod
 			res[i][0] = res[i][0] % 2;
 		}
